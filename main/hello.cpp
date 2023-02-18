6aa2bd4d902aa588d49cc515d514674d552a5f45
ArnavKumar7 <78247827+ArnavKumar7@users.noreply.github.com>
2023-02-18 22:47:47 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "i have made changes to the file now, my srn is PES2UG20CS403";
+  cout << "i have made changes to the file now, my SRN is PES2UG20CS403";
   return 0;
 }
